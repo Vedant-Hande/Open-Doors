c81cbae4d7107dba2e39000ea821a13369e772b0
vedant hande <vedanthande453@gmail.com>
2025-08-30 14:20:36 +0530

    use wrapAsync for error handling

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const Listing = require("./models/listing.js");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const app = express();
 const port = 8080;
@@ -17,10 +18,6 @@ app.use(methodOverride("_method"));
 
 //middleware to parse data
 app.use(express.urlencoded({ extended: true }));
-
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}`);
-});
 
 async function main() {
   await mongoose.connect("mongodb://localhost:27017/TripSpot");
@@ -40,10 +37,13 @@ main()
 // });
 
 // all listings route
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
 
 //new listing route - form to create new listing
 app.get("/listing/new", (req, res) => {
@@ -58,20 +58,23 @@ app.get("/listing/:id", async (req, res) => {
 });
 
 //create new listing route - handle form submission
-app.post("/listings", async (req, res) => {
-  let { title, desc, price, location, country, image } = req.body;
-  const newListing = new Listing({
-    title,
-    desc,
-    price,
-    location,
-    country,
-    "image.url": image.url,
-  });
-  await newListing.save();
-  console.log("New listing created:", newListing);
-  res.redirect("/listings");
-});
+app.post(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    let { title, desc, price, location, country, image } = req.body;
+    const newListing = new Listing({
+      title,
+      desc,
+      price,
+      location,
+      country,
+      "image.url": image.url,
+    });
+    await newListing.save();
+    console.log("New listing created:", newListing);
+    res.redirect("/listings");
+  })
+);
 
 //edit listing route - form to edit an existing listing
 app.get("/listing/:id/edit", async (req, res) => {
@@ -114,4 +117,13 @@ app.get("/auth/login", (req, res) => {
 
 app.get("/signup", (req, res) => {
   res.render("auth/signup.ejs");
+});
+
+// Error handling middleware -
+app.use((err, req, res, next) => {
+  res.send("some error");
+});
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
 });
