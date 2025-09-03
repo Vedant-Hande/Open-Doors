3cbf95df6968a482f331918b5fdabd088b26a103
vedant hande <vedanthande453@gmail.com>
2025-09-03 22:28:31 +0530

    refactor: Improve error handling in new listing form by adding validation for price input and enhancing user feedback for required fields.

---
+++ models/review.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.schema;
+
+const reviewSchema = new schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  comment: {
+    type: String,
+    maxLength: 250,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
