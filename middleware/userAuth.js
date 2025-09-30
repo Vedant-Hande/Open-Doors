c2963056feb40d624738f8bd46817cde8bffb250
vedant hande <vedanthande453@gmail.com>
2025-10-01 00:29:45 +0530

    refactor: Remove console log from user authentication middleware to clean up code

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -1,5 +1,4 @@
 module.exports.isLoggedIn = (req, res, next) => {
-  console.log(req.path, "..", req.originalUrl);
   if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
     req.flash("error", "You should logged in to List your place!");
