362b1db5e35b85e14f7c7e84791b26b398a72d36
vedant hande <vedanthande453@gmail.com>
2025-10-30 22:41:45 +0530

    refactor: Remove unused Listing model import from listingRoute.js

--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const Listing = require("../models/listing.js");
 const listingController = require("../controllers/listingController.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const {
